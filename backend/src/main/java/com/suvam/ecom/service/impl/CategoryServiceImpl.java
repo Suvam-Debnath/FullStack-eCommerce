aff8bed6eace237b39d03f37168b3fd3a31a1500
Suvam Debnath <debnathsuvam322@gmail.com>
2026-05-10 17:20:45 +0530

    added category

---
+++ backend/src/main/java/com/suvam/ecom/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,50 @@
+package com.suvam.ecom.service.impl;
+
+import com.suvam.ecom.model.Category;
+import com.suvam.ecom.repositories.CategoryRepository;
+import com.suvam.ecom.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@Service
+public class CategoryServiceImpl implements CategoryService {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Override
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
+    }
+
+    @Override
+    public void createCategory(Category category) {
+        categoryRepository.save(category);
+    }
+
+    @Override
+    public String deleteCategory(Long categoryId) {
+        Category category = categoryRepository.findById(categoryId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Resource not found"));
+
+        categoryRepository.delete(category);
+        return "Category with categoryId: " + categoryId + " deleted successfully !!";
+    }
+
+    @Override
+    public Category updateCategory(Category category, Long categoryId) {
+
+        Category savedCategory = categoryRepository.findById(categoryId)
+                .orElseThrow(() -> new ResponseStatusException(
+                        HttpStatus.NOT_FOUND,
+                        "Resource Not Found"));
+
+        savedCategory.setCategoryName(category.getCategoryName());
+
+        return categoryRepository.save(savedCategory);
+    }
+}
