d816c19edecce885ea6568392485bdc005f6e7af
Suvam Debnath <debnathsuvam322@gmail.com>
2026-05-31 09:22:11 +0530

    implemented User Address

---
+++ backend/src/main/java/com/suvam/ecom/service/AddressService.java
@@ -0,0 +1,20 @@
+package com.suvam.ecom.service;
+
+import com.suvam.ecom.model.User;
+import com.suvam.ecom.payload.AddressDTO;
+
+import java.util.List;
+
+public interface AddressService {
+    AddressDTO createAddress(AddressDTO addressDTO, User user);
+
+    List<AddressDTO> getAddresses();
+
+    AddressDTO getAddressesById(Long addressId);
+
+    List<AddressDTO> getUserAddresses(User user);
+
+    AddressDTO updateAddress(Long addressId, AddressDTO addressDTO);
+
+    String deleteAddress(Long addressId);
+}
