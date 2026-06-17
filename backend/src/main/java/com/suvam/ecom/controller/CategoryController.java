268868bff214cf9cb60b8439e22899e2274dde7c
Suvam Debnath <debnathsuvam322@gmail.com>
2026-06-17 22:51:34 +0530

    swagger

--- backend/src/main/java/com/suvam/ecom/controller/CategoryController.java
+++ backend/src/main/java/com/suvam/ecom/controller/CategoryController.java
@@ -4,6 +4,11 @@ import com.suvam.ecom.config.AppConstants;
 import com.suvam.ecom.payload.CategoryDTO;
 import com.suvam.ecom.payload.CategoryResponse;
 import com.suvam.ecom.service.CategoryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -16,6 +21,7 @@ public class CategoryController {
     @Autowired
     private CategoryService categoryService;
 
+    @Tag(name = "Category Controller", description = "APIs for managing category")
     @GetMapping("/public/categories")
     public ResponseEntity<CategoryResponse> getAllCategories(
             @RequestParam(name = "pageNumber", defaultValue = AppConstants.PAGE_NUMBER, required = false) Integer pageNumber,
@@ -26,6 +32,14 @@ public class CategoryController {
         return new ResponseEntity<>(categoryResponse, HttpStatus.OK);
     }
 
+    @Tag(name = "Category Controller", description = "APIs for managing category")
+    @Operation(summary = "Create category",
+            description = "API to create a new category")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "Category is created successfully"),
+            @ApiResponse(responseCode = "400", description = "Invalid input", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content)
+    })
     @PostMapping("/public/categories")
     public ResponseEntity<CategoryDTO> createCategory(@Valid @RequestBody CategoryDTO categoryDTO){
         CategoryDTO savedCategoryDTO = categoryService.createCategory(categoryDTO);
