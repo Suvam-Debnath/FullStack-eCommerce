7fce15c43ec2d5d8f53c39cba638e3d5236cfe97
Suvam Debnath <debnathsuvam322@gmail.com>
2026-08-02 20:38:10 +0530

    category and keyword filtering backend

--- frontend/src/components/Filter.jsx
+++ frontend/src/components/Filter.jsx
@@ -1,6 +1,7 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FiArrowDown, FiArrowUp, FiRefreshCw, FiSearch } from "react-icons/fi";
 import { Button, FormControl, InputLabel, MenuItem, Select, Tooltip } from "@mui/material";
+import { useLocation, useNavigate, useSearchParams } from "react-router-dom";
 
 const Filter = () => {
     const categories = [
@@ -9,10 +10,63 @@ const Filter = () => {
         {categoryId: 3, categoryName: 'Home & Garden'}
     ];
 
-    const [category,setCategory] = useState("all");
+    const [searchParams] = useSearchParams();
+    const params = new URLSearchParams(searchParams);
+    const pathname = useLocation().pathname;
+    const navigate = useNavigate();
+
+    const [category, setCategory] = useState("all");
+    const [sortOrder, setSortOrder] = useState("asc");
+    const [searchTerm, setSearchTerm] = useState("");
+
+    useEffect(() => {
+        const currentCategory = searchParams.get("category") || "all";
+        const currentSortOrder = searchParams.get("sortby") || "asc";
+        const currentSearchTerm = searchParams.get("keyword") || "";
+
+        setCategory(currentCategory);
+        setSortOrder(currentSortOrder);
+        setSearchTerm(currentSearchTerm);
+    }, [searchParams]);
+
+    useEffect(() => { 
+        const handler = setTimeout(() => {
+            if (searchTerm) {
+                searchParams.set("keyword", searchTerm);
+            } else {
+                searchParams.delete("keyword");
+            }
+            navigate(`${pathname}?${searchParams.toString()}`);
+        }, 700);
+
+        return () => {
+            clearTimeout(handler);
+        };
+    }, [searchParams, searchTerm, navigate, pathname]);
 
     const handleCategoryChange = (event) => {
+        const selectedCategory = event.target.value;
+
+        if (selectedCategory === "all") {
+            params.delete("category");
+        } else {
+            params.set("category", selectedCategory);
+        }
+        navigate(`${pathname}?${params}`);
         setCategory(event.target.value);
+    };
+
+    const toggleSortOrder = () => {
+        setSortOrder((prevOrder) => {
+            const newOrder = (prevOrder === "asc") ?  "desc" : "asc";
+            params.set("sortby", newOrder);
+            navigate(`${pathname}?${params}`);
+            return newOrder;
+        })
+    };
+
+     const handleClearFilters = () => {
+        navigate({ pathname : window.location.pathname });
     };
 
     return (
@@ -22,7 +76,7 @@ const Filter = () => {
                 <input 
                     type="text"
                     placeholder="Search Products"
-                    // value={searchTerm}
+                    value={searchTerm}
                     onChange={(e) => setSearchTerm(e.target.value)}
                     className="border border-gray-400 text-slate-800 rounded-md py-2 pl-10 pr-4 w-full focus:outline-hidden focus:ring-2 focus:ring-[#1976d2]"/>
                     <FiSearch className="absolute left-3 text-slate-800 size={20}"/>
@@ -54,22 +108,22 @@ const Filter = () => {
                  {/* SORT BUTTON & CLEAR FILTER */}
                 <Tooltip title="Sorted by price: asc">
                     <Button variant="contained" 
-                        //onClick={toggleSortOrder}
+                        onClick={toggleSortOrder}
                         color="primary" 
                         className="flex items-center gap-2 h-10">
                         Sort By
-                        {/* {sortOrder === "asc" ? (
+                        {sortOrder === "asc" ? (
                             <FiArrowUp size={20} />
                         ) : (
                             <FiArrowDown size={20} />
-                        )} */}
+                        )}
                         
                     </Button>
                 </Tooltip>
 
                 <button 
                 className="flex items-center gap-2 bg-rose-900 text-white px-3 py-2 rounded-md transition duration-300 ease-in shadow-md focus:outline-hidden"
-                //onClick={handleClearFilters}
+                onClick={handleClearFilters}
                 >
                     <FiRefreshCw className="font-semibold" size={16}/>
                     <span className="font-semibold">Clear Filter</span>
