6c411e2008554b1ae864b066ed04f3ee2d2f39c6
Suvam Debnath <debnathsuvam322@gmail.com>
2026-08-07 17:03:07 +0530

    home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,25 @@
 import './App.css'
 import Products from './components/products/Products'
 import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from './components/home/Home'
+import Navbar from './components/shared/Navbar'
+import About from './components/About'
+import Contact from './components/Contact'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <Products/>
-    </>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={ <Home />}/>
+        <Route path='/products' element={ <Products />}/>
+        <Route path='/about' element={ <About />}/>
+        <Route path='/contact' element={ <Contact />}/>
+      </Routes>
+    </Router>
   )
 }
 
