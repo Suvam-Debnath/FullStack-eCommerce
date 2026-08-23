68f461632fbc9968fe8cb230a59db1d53a9eac9b
Suvam Debnath <debnathsuvam322@gmail.com>
2026-08-23 19:52:57 +0530

    logout

--- frontend/src/store/actions/index.js
+++ frontend/src/store/actions/index.js
@@ -142,3 +142,9 @@ export const registerNewUser
             setLoader(false);
         }
     };
+
+export const logOutUser = (navigate) => (dispatch) => {
+    dispatch({ type: "LOG_OUT" });
+    localStorage.removeItem("auth");
+    navigate("/login");
+};
